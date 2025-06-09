cab582f93225f6a750dd3dad93c44c2b2f1df96d
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2025-06-09 07:36:22 -0500

    Delete serverless directory

--- serverless/main.tf
+++
@@ -1,8 +0,0 @@
-data "azurerm_resource_group" "main" {
-  name = "vmbased-vnet"
-}
-
-resource "azurerm_resource_group" "main" {
-  name = data.azurerm_resource_group.main.name
-  location = data.azurerm_resource_group.main.location
-}
