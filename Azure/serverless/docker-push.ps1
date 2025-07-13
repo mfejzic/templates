fc4ced958a1d666ce48800aac45ce9ab631da3ff
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2025-07-13 17:54:11 -0500

    Update docker-push.ps1

--- Azure/serverless/docker-push.ps1
+++ Azure/serverless/docker-push.ps1
@@ -1,4 +1,4 @@
-# will auto push docke rimage to registry after its creation
+# will auto push docke rimage to registry after its creation - container app wil auto pull this image
 
 az acr login --name mf37registry
 docker tag flask-cosmos-app:latest mf37registry.azurecr.io/flask-cosmos-app:latest
