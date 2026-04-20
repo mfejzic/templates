3f847f3c666ba4e7f2669d344523227eae369254
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2026-04-20 17:48:12 -0500

    Delete init.sql

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100)
-);
-
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Bob');
-INSERT INTO users (name) VALUES ('John');
